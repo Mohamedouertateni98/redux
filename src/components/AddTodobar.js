74c46e0a91273fe978cb26f014fa3c12f3fa91d6
mohamed <ouertateni1998@gmail.com>
2022-09-28 19:23:15 +0100

    sec commit

--- src/components/AddTodobar.js
+++ src/components/AddTodobar.js
@@ -8,8 +8,8 @@ function AddTodobar() {
     
     return (
         <section class="container2">
-            <input onKeyDown={((e) => e.keyCode === 13 ? Typed === "" ? alert("your todo is empty") : dispatch(addTodoList({ id: Math.random(), Title: Typed, checked: false, Done: false, show: true })) && updateTyped("") : NaN)} placeholder='Any plans for today?' value={Typed} onChange={(e) => updateTyped(e.target.value)}></input>
-            <button onClick={(e) => Typed === "" ? alert("your todo is empty") : dispatch(addTodoList({ id: Math.random(), Title: Typed, checked: false, Done: false, show: true })) && updateTyped("")}>Add</button>
+            <input onKeyDown={((e) => e.keyCode === 13 ? Typed === "" ? alert("your todo is empty") : dispatch(addTodoList({ id: Math.random(), Title: Typed, checked: false, Done: false, show: true })) && updateTyped("") : NaN)} placeholder='write your todo here...' value={Typed} onChange={(e) => updateTyped(e.target.value)}></input>
+            <button onClick={(e) => Typed === "" ? alert("your todo is empty") : dispatch(addTodoList({ id: Math.random(), Title: Typed, checked: false, Done: false, show: true })) && updateTyped("")}>Add Todo</button>
         </section>
     )
 }
